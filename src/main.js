688b01bef0e91fb51d828ec2e9cff9e89e24b2db
Gordon Doskas <gordon2012@users.noreply.github.com>
2016-05-11 23:10:29 -0700

    The Bird

--- src/main.js
+++ src/main.js
@@ -3,17 +3,51 @@ var mainState = {
     preload: function() {
         // This function will be executed at the beginning
         // That's where we load the images and sounds
+        game.load.image('bird', 'https://s3-us-west-2.amazonaws.com/s.cdpn.io/303256/bird.png');
     },
 
     create: function() {
         // This function is called after the preload function
         // Here we set up the game, display sprites, etc.
+        game.stage.backgroundColor = '#71c5cf';
+
+        game.physics.startSystem(Phaser.Physics.ARCADE);
+
+        this.bird = game.add.sprite(100, 245, 'bird');
+
+        game.physics.arcade.enable(this.bird);
+
+        this.bird.body.gravity.y = 1000;
+
+        var spaceKey = game.input.keyboard.addKey(Phaser.Keyboard.SPACEBAR);
+        spaceKey.onDown.add(this.jump, this);
+
+
+
+
     },
 
     update: function() {
         // This function is called 60 times per second
         // It contains the game's logic
+        if (this.bird.y < 0 || this.bird.y > 490)
+        this.restartGame();
     },
+
+
+    // Make the bird jump
+    jump: function() {
+    // Add a vertical velocity to the bird
+    this.bird.body.velocity.y = -350;
+    },
+
+    // Restart the game
+    restartGame: function() {
+        // Start the 'main' state, which restarts the game
+        game.state.start('main');
+    },
+
+
 };
 
 // Initialize Phaser, and create a 400px by 490px game
